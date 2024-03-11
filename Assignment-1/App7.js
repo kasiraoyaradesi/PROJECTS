0b99dfea3d2d14d5e113cf3041eefc1f6b566f5c
kasiraoyaradesi <you@example.com>
2024-03-11 11:32:38 +0530

    first commit

---
+++ Assignment-1/App7.js
@@ -0,0 +1,20 @@
+import React from 'react';
+ import './App.css';
+import App6 from './App6';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+  <div className='App'>
+    <Header />
+    <hr></hr>
+    <App6 />
+    <hr></hr>
+    <Footer />  
+                                      
+    </div>
+  );
+}
+
+export default App;
