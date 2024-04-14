1f8207fc2fe066fc97b787e11574c7ab9d963c79
kasiraoyaradesi <you@example.com>
2024-04-14 14:57:33 +0530

    1

---
+++ Coffe/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App1 from './App1';
+// import Calc from './Calc';
+// import Result from './Result';
+// import App2 from './App2'
+// import App3 from './App3'
+
+// import App4 from './App4'
+
+// import App5 from './App5'
+
+// import App6 from './App6'
+
+// import Product from './Product'
+
+// import Coffe from './Coffe'
+
+// import App8 from './App8'
+
+// import App9 from './App9'
+
+import App13a from './App13a'
+
+
+// import Login1 from './Login1'
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     {/* <App1 name= "Jhon" age = {34} /> */}
+
+   {/* <Calc  a= {2}  b = {4} /> */}
+
+   {/* < Result marks = {20}/> */}
+   {/* {/ <App2 user ="" />   if use name then coming name only otherwise box} */}
+
+
+  {/* <App3 name = {["jhon","cathy"]}/> */}
+
+  {/* <App5 student = {{name:"jhon", age:28}}/> */}
+
+
+  {/* <App5/> */}
+  {/* <App6/> */}
+
+  {/* <Coffe/> */}
+
+  {/* <App8 /> */}
+
+  {/* <App9 /> */}
+
+
+  {/* <App11 /> */}
+
+  {/* <Login1/> */}
+
+  <App13a/>
+
+
+  </React.StrictMode>
+);
+
